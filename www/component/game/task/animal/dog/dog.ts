c327251f215cb1d5fe8f19515e24b3bd0f285e9b
dim <web.best.master@gmail.com>
2022-01-17 20:50:33 +0100

    update

--- www/component/game/task/animal/dog/dog.ts
+++ www/component/game/task/animal/dog/dog.ts
@@ -6,7 +6,7 @@ import dogImage1 from './image/dog-1.svg';
 import dogSound1 from './sound/dog-1.mp3';
 
 export const dogAnimal: AnimalType = {
-    foodIdList: [FoodEnum.dogBowlFood, FoodEnum.dogCanFood],
+    foodIdList: [FoodEnum.dogBowlFood, FoodEnum.dogCanFood, FoodEnum.meat],
     id: AnimalEnum.dog,
     imageList: [dogImage1],
     soundList: [dogSound1],
