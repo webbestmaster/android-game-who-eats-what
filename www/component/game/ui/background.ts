58531e6db10b1d505292162422e2a51fed79dd0f
dim <web.best.master@gmail.com>
2022-01-18 21:59:27 +0100

    update

--- www/component/game/ui/background.ts
+++ www/component/game/ui/background.ts
@@ -1,6 +1,6 @@
 import background1 from './background/background-1.jpg';
 import background2 from './background/background-2.png';
-import background3 from './background/background-3.jpg';
+import background3 from './background/background-3.png';
 import background4 from './background/background-4.png';
 
 export const backgroundList: Array<string> = [background1, background2, background3, background4];
